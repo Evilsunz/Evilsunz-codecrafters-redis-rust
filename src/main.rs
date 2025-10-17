9db4f8e8c4804c106b2b23ede2c4f8f69980cdf7
Maxim <maxim_dmytrenko@comcast.com>
2025-10-17 16:48:08 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 #![allow(unused_imports)]
 
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    match listener.accept() {
-        Ok((mut stream, _)) => {
-            loop {
-                let mut buffer = [0; 512];
-                stream.read(&mut buffer).unwrap();
-                if buffer.is_empty() {
-                    stream.shutdown(std::net::Shutdown::Both).unwrap();
-                    break;
-                }
-                stream.write_all(b"+PONG\r\n").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_stream(stream.try_clone().unwrap());
+                });
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
-        Err(e) => {
-            println!("error: {}", e);
+    }
+
+    fn handle_stream(mut stream: TcpStream) {
+        loop {
+            let mut buffer = [0; 512];
+            stream.read(&mut buffer).unwrap();
+            if buffer.is_empty() {
+                stream.shutdown(std::net::Shutdown::Both).unwrap();
+                break;
+            }
+            stream.write_all(b"+PONG\r\n").unwrap();
         }
     }
 }
