5c338320dda6eb4064c74b524056694ae9076dd1
Maxim <maxim_dmytrenko@comcast.com>
2025-10-28 12:19:30 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::str::FromStr;
 use std::thread;
+use clap::Parser;
 use codecrafters_redis::{decode_resp_array, encode_string, Handler, TXContext};
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 6379)]
+    port: u16,
+}
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let args = Args::parse();
+    let mut listener = TcpListener::bind(SocketAddr::new(IpAddr::from_str("127.0.0.1").unwrap(), args.port)).unwrap();
 
     for stream in listener.incoming() {
         match stream {
