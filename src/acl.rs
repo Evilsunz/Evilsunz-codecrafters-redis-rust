37bed7384dc79e722d8c2775b9088823f46ce139
Maxim <maxim_dmytrenko@comcast.com>
2025-11-08 16:43:44 +0200

    codecrafters submit [skip ci]

--- src/acl.rs
+++ src/acl.rs
@@ -2,8 +2,9 @@ use resp::Value;
 use crate::{encode_null, encode_vec_of_value};
 
 const FLAGS: &str = "flags";
+const NOPASS: &str = "nopass";
 
 pub fn get_user() -> Vec<u8> {
-    let user_info = vec![Value::Bulk(FLAGS.to_string()), Value::Array(vec![])];
+    let user_info = vec![Value::Bulk(FLAGS.to_string()), Value::Array(vec![Value::Bulk(NOPASS.to_string())])];
     encode_vec_of_value(user_info)
 }
